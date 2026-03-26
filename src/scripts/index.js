d8a85dc9b874819b9279b752017a3ee5ebcd04d1
Pavel <yashchenkodev@gmail.com>
2026-03-26 16:37:37 +0200

    feat: add about section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,36 +1,59 @@
-const headerBurger = document.querySelector('.header__burger');
-
-headerBurger.addEventListener('click', () => headerBurger.classList.toggle('is-active'));
-
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(ScrollTrigger);
+function burgerMenuOpen() {
+  const headerBurger = document.querySelector('.header__burger');
+  headerBurger.addEventListener('click', () => headerBurger.classList.toggle('is-active'));
+}
 
-gsap.fromTo(
-  '.car',
-  { y: '-70%' },
-  {
-    y: '50%',
-    ease: 'none',
+function sectionAboutCarAnimationInit() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // car animation
+  gsap.fromTo(
+    '.about__animation-car',
+    { y: '-70%' },
+    {
+      y: '50%',
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '.about__animation',
+        start: 'top 50%',
+        end: 'center center',
+        scrub: true,
+      },
+    },
+  );
+
+  // bg animation
+  const introTl = gsap.timeline({
     scrollTrigger: {
-      trigger: '.scene',
+      trigger: '.about__animation',
       start: 'top 50%',
-      end: 'center center',
+      end: 'top 20%',
       scrub: true,
     },
-  },
-);
+  });
 
-const introTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.scene',
-    start: 'top 50%',
-    end: 'top 20%',
-    scrub: true,
-  },
+  introTl.to(
+    '.about__animation-overlay',
+    {
+      y: '80%',
+      ease: 'none',
+    },
+    0,
+  );
+  introTl.to(
+    '.about__animation-bg',
+    {
+      y: '10%',
+      ease: 'none',
+    },
+    0,
+  );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  burgerMenuOpen();
+  sectionAboutCarAnimationInit();
 });
-introTl.to('.overlay', {
-  y: '50%',
-  ease: 'none',
-}, 0);
