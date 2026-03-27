4eb2b8658b6b4914f1e59eb7103e6ee7e131ebba
Pavel <yashchenkodev@gmail.com>
2026-03-27 05:20:04 +0200

    feat: finish desktop version

--- src/scripts/headerMenuBurger.js
+++ src/scripts/headerMenuBurger.js
@@ -1,4 +1,17 @@
 export function burgerMenuOpen() {
   const headerBurger = document.querySelector('.header__burger');
-  headerBurger.addEventListener('click', () => headerBurger.classList.toggle('is-active'));
+
+  headerBurger.addEventListener('click', (e) => {
+    e.stopPropagation();
+    headerBurger.classList.toggle('is-active');
+  });
+
+  document.addEventListener('click', (e) => {
+    const isClickInsideBurger = e.target.closest('.header__burger');
+    const isClickInsideMenu = e.target.closest('.header__nav');
+
+    if (!isClickInsideBurger && !isClickInsideMenu) {
+      headerBurger.classList.remove('is-active');
+    }
+  });
 }
