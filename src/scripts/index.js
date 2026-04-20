9bef4ce423cdafe31fa83eb64fe13cc56e6139c4
Pavel <yashchenkodev@gmail.com>
2026-04-20 21:05:12 +0300

    feat: add hero animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,16 @@ import { burgerMenuOpen } from './headerMenuBurger';
 import { sectionAboutCarAnimationInit } from './aboutAnimation';
 import { servicesSlider } from './servicesSlider';
 import { featuredSlider } from './featuredSlider';
+import AOS from 'aos';
+
+import 'aos/dist/aos.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   burgerMenuOpen();
   sectionAboutCarAnimationInit();
   servicesSlider();
   featuredSlider();
+  AOS.init({
+    once: true,
+  });
 });
