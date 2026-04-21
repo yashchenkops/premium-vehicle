0f186ec5283d786066fb5cbdb8e1b4f39a16e3ef
Pavel <yashchenkodev@gmail.com>
2026-04-21 10:03:38 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: '/premium-vehicle/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
