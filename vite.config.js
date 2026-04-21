efad1a8a684a91cd753cd60eac08ffeb01264d84
Pavel <yashchenkodev@gmail.com>
2026-04-21 09:43:18 +0300

    build 2.0

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/premium-vehicle/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
